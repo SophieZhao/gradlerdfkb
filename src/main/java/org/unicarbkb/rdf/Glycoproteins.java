8d54daab744471d5b7ecc58ac3ba488ee002a27f
alternativeTime <matthew.campbell1980@gmail.com>
2014-05-22 20:18:00 +1000

    first release ready - onto experimental data

--- src/main/java/org/unicarbkb/rdf/Glycoproteins.java
+++ src/main/java/org/unicarbkb/rdf/Glycoproteins.java
@@ -9,9 +9,11 @@ import com.hp.hpl.jena.vocabulary.RDF;
 import models.database.*;
 import org.unicarbkb.rdf.StructureRDF;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 
+import static java.net.URLEncoder.encode;
 import static org.unicarbkb.rdf.StructureRDF.*;
 
 
@@ -35,7 +37,7 @@ public class Glycoproteins {
             }
 
             if (protein.swissProt == null && protein.name != null) {
-                Resource p = model.createResource("http://rdf.unicarbkb.org/proteinsummary/" + URLEncoder.encode(protein.getName().trim(), "UTF-8"));
+                Resource p = model.createResource("http://rdf.unicarbkb.org/proteinsummary/" + encode(protein.getName().trim(), "UTF-8"));
                 p.addProperty(GLYCOVOCAB.hasProteinName, protein.getName().trim());
 
                 List<Stproteins> strprotein = protein.stproteins;
@@ -66,47 +68,71 @@ public class Glycoproteins {
     REPLACE aSNASN
      */
     public static Resource createGlycoAA(DefinedSites d, Model model) {
-        String glycoAAURI = "http://rdf.unicarbkb.org/glycoAASite/" + d.proteins.id + d.amino_acid_position; //bad + JenaUUID.generate().asString();
         Resource r = null;
-        try {
-            r = model.createResource(glycoAAURI);
-            r.addProperty(FALDO.Location, createFaldoLocation(d.amino_acid_position, model));
-            r.addProperty(RDF.type, GLYCOVOCAB.glycosylatedAA);
+        //work area
 
-            List<StructureToSiteDefined> sD = d.strSiteDefined;
-            for (StructureToSiteDefined s : sD) {
-                System.out.println("structure dead is " + s.structure_id);
-                r.addProperty(GLYCOVOCAB.hasAttachedGlycan, createStructureFromDefinedSite(model, s.structure_id));
-            }
+        List<StructureToSiteDefined> t = d.strSiteDefined;
+        System.out.println("this is the protein: " + d.protein_name + " with am string " + d.amino_acid_position );
 
-            if (d.amino_acid_position.contains("ASN")) {
-                r.addProperty(GLYCOVOCAB.aminoAcidType, model.getResource("asparagine")); //TODO CHECK
-                r.addLiteral(GLYCOVOCAB.modificationType, "N-glycosylation");
+        for (StructureToSiteDefined z : t) {
+
+            String amino = z.amino_acid_position;
+            String[] aa = amino.split(("(?i)AND|OR|AND/OR|,|IN|ON|OF|\\&"));
+            for (String aaa : aa) {
+                String aaaa = aaa.toUpperCase().replaceAll("AsnAsn", "Asn").replaceAll("SerSer", "Ser").replaceAll("ThrThr", "Thr").replaceAll("\\(.+?\\)", "").replaceAll("\\-", "");
+                aaaa.replaceAll("ASNASN","ASN").replaceAll("\\s+","").replaceAll("\\(MAJ", "");
+
+                System.out.println("split amino string: " + aaa.toString().trim());
+
+                String glycoAAURI = "http://rdf.unicarbkb.org/glycoAASite/" + d.proteins.id + "_" + aaaa.replaceAll("ASNASN","ASN").replaceAll("\\s+","").replaceAll("\\(MAJ", "").trim(); //bad + JenaUUID.generate().asString();
+
+                try {
+                    r = model.createResource(glycoAAURI);
+                    r.addProperty(FALDO.Location, createFaldoLocation(aaaa.trim(), z.protein_name.trim(), model, z));
+                    r.addProperty(RDF.type, GLYCOVOCAB.glycosylatedAA);
+
+                    List<StructureToSiteDefined> sD = d.strSiteDefined;
+                    for (StructureToSiteDefined s : sD) {
+                        System.out.println("structure dead is " + s.structure_id);
+                        r.addProperty(GLYCOVOCAB.hasAttachedGlycan, createStructureFromDefinedSite(model, s.structure_id));
+                    }
+
+                    if (z.amino_acid_position.contains("ASN")) {
+                        r.addProperty(GLYCOVOCAB.aminoAcidType, model.getResource("asparagine")); //TODO CHECK
+                        r.addLiteral(GLYCOVOCAB.modificationType, "N-glycosylation");
+                    }
+                    if (z.amino_acid_position.contains("THR")) {
+                        r.addProperty(GLYCOVOCAB.aminoAcid, model.getResource("threonine")); //TODO CHECK
+                        r.addLiteral(GLYCOVOCAB.modificationType, "O-glycosylation");
+                    }
+                    if (z.amino_acid_position.contains("SER")) {
+                        r.addProperty(GLYCOVOCAB.aminoAcid, model.getResource("serine")); //TODO CHECK
+                        r.addLiteral(GLYCOVOCAB.modificationType, "O-glycosylation");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Failed creating glycoAA: " + e);
+                }
+                //model.write(System.out, "TTL");
             }
-            if (d.amino_acid_position.contains("THR")) {
-                r.addProperty(GLYCOVOCAB.aminoAcid, model.getResource("threonine")); //TODO CHECK
-                r.addLiteral(GLYCOVOCAB.modificationType, "O-glycosylation");
-            }
-            if (d.amino_acid_position.contains("SER")) {
-                r.addProperty(GLYCOVOCAB.aminoAcid, model.getResource("serine")); //TODO CHECK
-                r.addLiteral(GLYCOVOCAB.modificationType, "O-glycosylation");
-            }
-        } catch (Exception e) {
-            System.out.println("Failed creating glycoAA: " + e);
         }
-        //model.write(System.out, "TTL");
         return r;
     }
 
     /*
     Pass glycoprotein glycosylation site position
      */
-    public static Resource createFaldoLocation(String position, Model model) {
-        String faldoURI = "http://rdf.unicarbkb.org/position/" + JenaUUID.generate().asURI();
+    public static Resource createFaldoLocation(String position, String protein, Model model, StructureToSiteDefined z) throws UnsupportedEncodingException {
+        String faldoURI = null;
+        if(z.definedSites.proteins.swissProt != null) {
+            faldoURI = "http://rdf.unicarbkb.org/position/" + z.definedSites.proteins.swissProt.trim() + "_" + position.replaceAll("\\-", "").replaceAll("ASNASN","ASN").replaceAll("\\s+","").replaceAll("\\(MAJ", "").trim(); // + JenaUUID.generate().asURI();
+        }
+        if(z.definedSites.proteins.swissProt == null && z.protein_name != null) {
+            faldoURI = "http://rdf.unicarbkb.org/position/" + encode(z.protein_name.trim(), "UTF-8") + "_" + position.replaceAll("\\-", "").trim();
+        }
         Resource r = null;
         try {
             r = model.createResource(faldoURI);
-            r.addLiteral(FALDO.position, position.replaceAll("ASN-", "").replaceAll("SER-", "").replaceAll("THR-", ""));
+            r.addLiteral(FALDO.position, position.replaceAll("ASN-", "").replaceAll("SER-", "").replaceAll("THR-", "").replaceAll("ASNASN", ""));
         } catch (Exception e) {
             System.out.println("Failed on Faldo: " + e);
         }
