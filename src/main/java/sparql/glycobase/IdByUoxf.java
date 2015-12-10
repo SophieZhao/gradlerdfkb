8b647bc42092eb089302be79b378f5b774f88139
SophieZhao <SophieZhao@users.noreply.github.com>
2015-12-10 22:04:28 +1100

    now all search return uoxf and glycobaseid

--- src/main/java/sparql/glycobase/IdByUoxf.java
+++ src/main/java/sparql/glycobase/IdByUoxf.java
@@ -1,10 +1,12 @@
 package sparql.glycobase;
 
+import sparql.SelectSparql;
 import sparql.SelectSparqlBean;
 import sparql.SparqlException;
 
 
 public class IdByUoxf extends SelectSparqlBean implements GlycanGlycobase {
+    public static final String Uoxf=GlycanGlycobase.Uoxf;
 
     public IdByUoxf(String sparql) {
         super(sparql);
@@ -13,7 +15,7 @@ public class IdByUoxf extends SelectSparqlBean implements GlycanGlycobase {
     public IdByUoxf() {
         super();
         this.prefix = "PREFIX glycan: <http://purl.jp/bio/12/glyco/glycan/> \n";  //need to add # glycan#
-        this.select = "DISTINCT ?" + GlycoBaseId + "\n";
+        this.select = "DISTINCT ?" + GlycoBaseId + " ?" + Uoxf + "\n";
     }
 
     public String getUoxf() {return "\"" + getSparqlEntity().getValue(GlycanGlycobase.Uoxf) + "\"";}
@@ -22,6 +24,7 @@ public class IdByUoxf extends SelectSparqlBean implements GlycanGlycobase {
     @Override
     public String getWhere() throws SparqlException {
         String where = "?" + SaccharideURI + " glycan:has_uoxf " + getUoxf() + ";\n"
+                + " glycan:has_uoxf ?" + Uoxf + " ;\n"
                 + " glycan:has_glycobase_id ?" + GlycoBaseId + " .\n";
 
         return where;
